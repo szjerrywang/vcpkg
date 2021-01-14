61384a281eb4a939f2acd2b9ec15e45b05dc04c3
Jerry Wang <jerry.wang@alphaliongroup.com>
2021-01-14 11:18:33 +0800

    modify quantlib port file to install new version and with linux

--- ports/quantlib/portfile.cmake
+++ ports/quantlib/portfile.cmake
@@ -1,25 +1,26 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lballabio/QuantLib
-    REF f09141b5cce9134c0bcdbaf36e81359e6ba30705
-    SHA512 d4b19d33594a7072a0d90b7eac3d74fb27c526269713a9223b84c0451b1e06a58f0c98350305d68a55086d1971260ff249049112aaadea59397ec195a3291490
+    REF QuantLib-v1.20
+    SHA512 3929f339f5c626ae4e39ac476e1478600f1457240533a885743782e8abfcee0ef4e79518096f3bec21c952b6f812d40648a86b6a0e00ba87bd67644f8ed0ac56
     HEAD_REF master
     PATCHES
-        disable-examples-tests.patch
+        make-linux-build.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" USE_BOOST_DYNAMIC_LIBRARIES)
 
 set(QL_MSVC_RUNTIME ${VCPKG_LIBRARY_LINKAGE})
 
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+#in linux, it should be ok to build dynamic library
+#vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 # TODO: Fix it in the upstream
-vcpkg_replace_string(
-    "${SOURCE_PATH}/ql/userconfig.hpp"
-    "//#    define QL_USE_STD_UNIQUE_PTR"
-    "#    define QL_USE_STD_UNIQUE_PTR"
-)
+#vcpkg_replace_string(
+#    "${SOURCE_PATH}/ql/userconfig.hpp"
+#    "//#    define QL_USE_STD_UNIQUE_PTR"
+#    "#    define QL_USE_STD_UNIQUE_PTR"
+#)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
